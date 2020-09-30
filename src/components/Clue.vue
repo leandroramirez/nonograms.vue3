c38d65ec84e0bd6ced4b784fd1afd95870436a74
Leandro Ramirez <leandro.ramirez@daimler.com>
2020-09-30 20:15:22 -0300

    Fix foreignObject in svg cross-browsers compatibility

--- src/components/Clue.vue
+++ src/components/Clue.vue
@@ -10,12 +10,15 @@ export default {
 <template>
   <svg
     viewBox="0 0 100 100"
+    version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     xmlns:xhtml="http://www.w3.org/1999/xhtml"
     class="nng-square"
     :style="`background: ${clue.realColor}; color: ${clue.textColor};`"
   >
-    <foreignObject x="0" y="0" width="100%" height="100%">{{ clue.length }}</foreignObject>
+    <foreignObject x="0" y="0" width="100%" height="100%">
+      <xhtml:span>{{ clue.length }}</xhtml:span>
+    </foreignObject>
   </svg>
 </template>
